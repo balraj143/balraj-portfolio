2b4c67343a61c83f1b7a23bc893e56f886de28ed
balraj143 <147017712+balraj143@users.noreply.github.com>
2024-11-27 00:40:53 -0800

    Add files via upload

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "INTERSHIP :",
+        subTitle: "GALWIN-TECHNOLOGY",
+        description:
+          " Created and maintained static and dynamic web pages using HTML,CSS, JS. | JULY-2024",
+      },
+      {
+        title: "PROJECT :",
+        subTitle: "AI-CHATBOT",
+        description:
+          "I've creted the AI based chat-bot using html ,css and js here we have any doubt ask to bot and get answer. | OCT-2024",
+      },
+      {
+        title: "PROJECT :",
+        subTitle: "PERSONAL PORTFOLIO",
+        description:
+          "I've created the personal portfolio to showcase my project and about my skills .| DEC-2024",
+      },
+    ],
+    education: [
+      {
+        title: "ADW HR SEC SCHOOL : ",
+        subTitle: "10'TH",
+        description:
+          "75%  | APRIL-2019",
+      },
+      {
+        title: "ADW HR SEC SCHOOL :",
+        subTitle: "12'TH",
+        description:
+          "80% | APRIL-2021",
+      },
+      {
+        title: "BACHELOR OF ENGINEERING :",
+        subTitle: "COMPUTER SCIENCE",
+        description:
+          "CGPA : 8.00",
+      },
+    ],
+  };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,61 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "80",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "75",
+        },
+        {
+          skillName: "BOOTSTRAP",
+          percentage: "50",
+        },
+        {
+          skillName: "REACT",
+          percentage: "50",
+        },
+      ],
+    },
+  //   {
+  //     label: "BACK END",
+  //     data: [
+  //       {
+  //         skillName: "JAVA",
+  //         percentage: "70",
+  //       },
+  //       {
+  //         skillName: "PYTHON",
+  //         percentage: "65",
+  //       },
+  //       {
+  //         skillName: "KOTLIN",
+  //         percentage: "35",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     label: "DATABASES",
+  //     data: [
+  //       {
+  //         skillName: "SQL",
+  //         percentage: "70",
+  //       },
+  //       {
+  //         skillName: "MONGO DB",
+  //         percentage: "65",
+  //       },
+  //       {
+  //         skillName: "ORACLE",
+  //         percentage: "85",
+  //       },
+  //     ],
+  //   },
+   ];
